c5e2108d2fc161ad65873cc2c051b469f42514af
John Khoo <john_khoo@u.nus.edu>
2019-08-23 17:53:00 +0800

    Add say function and echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.lang.*;
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+        
+        String[] intro = {"Hello, I'm Duke!", "What can I do for you?"};
+
+        say(intro);
+
+        String[] input = {""};
+        Scanner scanIn = new Scanner(System.in);
+        while (true) {
+          input[0] = scanIn.nextLine();
+          say(input);
+        }
+    }
+
+    private static void say(String[] msg) {
+      String line = "____________________________________________________________";
+      String indent = "    ";
+      System.out.println(indent + line);
+      int lineCount = msg.length;
+      for (int i = 0; i < lineCount; ++i) {
+        System.out.println(indent + msg[i]);
+      }
+      System.out.println(indent + line);
+      System.out.println("");
     }
 }
