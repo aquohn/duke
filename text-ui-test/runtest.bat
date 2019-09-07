1aac8cc7e4232f9f6b1a0b3db5def1b3dcd214eb
John Khoo <john_khoo@u.nus.edu>
2019-09-07 10:24:59 +0800

    Fix runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -18,7 +18,7 @@ for /F "tokens=*" %%f in ('dir ..\src\main\java /B/A:-D/S "*.java" 2^>nul') do (
 )
 
 REM compile the code into the bin folder
-javac  -cp ..\bin\production -Xlint:none -d ..\bin\production @all.javas
+javac -cp ..\bin\production -Xlint:none -d ..\bin\production @all.javas
 IF ERRORLEVEL 1 (
    echo ********** BUILD FAILURE **********
     exit /b 1
@@ -30,19 +30,19 @@ cd ..
 
 REM run the program, feed commands from input.txt file and redirect the output
 REM to test.out
-java -classpath bin\production\duke duke/Duke < text-ui-test\input.txt > text-ui-test\ACTUAL.txt
+java -classpath bin\production duke/Duke < text-ui-test\input.txt > text-ui-test\ACTUAL.txt
 
 REM test recovery of corrupted file
 echo "file corrupted!" > data/tasks.tsv
 echo n > text-ui-test\n
-java -classpath bin\production\duke duke/Duke < text-ui-test\n >> text-ui-test\ACTUAL.txt
+java -classpath bin\production duke/Duke < text-ui-test\n >> text-ui-test\ACTUAL.txt
 del text-ui-test\n
 
 REM test deletion of corrupted file
 echo "file corrupted!" > data/tasks.tsv
 echo y > text-ui-test\ybye
 echo bye >> text-ui-test\ybye
-java -classpath bin\production\duke duke/Duke < text-ui-test\ybye >> text-ui-test\ACTUAL.txt
+java -classpath bin\production duke/Duke < text-ui-test\ybye >> text-ui-test\ACTUAL.txt
 del text-ui-test\ybye
 
 REM compare the output to the expected output
